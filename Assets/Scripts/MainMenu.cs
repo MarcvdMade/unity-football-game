e83f8f3fa35761e02445045fa4b96c8138c28687
Marc vd Made <0996549@hr.nl>
2022-02-12 00:38:28 +0100

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Play Game button
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit button
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    // Fullscreen toggle
+    public void FullscreenToggle (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
